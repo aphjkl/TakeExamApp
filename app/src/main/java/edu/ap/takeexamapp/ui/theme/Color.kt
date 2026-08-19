7ff1d380b313ba0f3ff01a9a3d04416f64a1bd17
AdamLaptop <adam.bakhmadov@student.ap.be>
2026-08-19 17:16:04 +0200

    update colors

--- app/src/main/java/edu/ap/takeexamapp/ui/theme/Color.kt
+++ app/src/main/java/edu/ap/takeexamapp/ui/theme/Color.kt
@@ -2,10 +2,13 @@ package edu.ap.takeexamapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ApRed = Color(0xFFE30613)
+val ApDarkRed = Color(0xFFAE0F0A)
+val ApGrey = Color(0xFF706F6F)
+val ApNearBlack = Color(0xFF191919)
+val ApDarkGrey = Color(0xFF454545)
+val ApOutline = Color(0xFF6B6B6B)
+val ApBackground = Color(0xFFFFFBFA)
+val ApSurface = Color(0xFFFFFFFF)
+val ApRedContainer = Color(0xFFFFDAD8)
+val ApOnRedContainer = Color(0xFF410003)
